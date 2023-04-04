31ab205aec5427d532817965e35d461bd74cb9ab
Yirmel-Sanchez <yirmelaugusto.sanchez@alu.uclm.es>
2023-04-04 18:35:14 +0200

    Creacion backend juegos

---
+++ src/main/java/edu/uclm/esi/gamesgames/GamesGamesApplication.java
@@ -0,0 +1,13 @@
+package edu.uclm.esi.gamesgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uclm/esi/gamesgames/GamesGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uclm.esi.gamesgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
